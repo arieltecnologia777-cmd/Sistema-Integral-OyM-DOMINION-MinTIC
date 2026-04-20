0bab8035ae280766deeabe33cfa4f8820151a417
arieltecnologia777-cmd <arieltecnologia777@gmail.com>
2026-04-19 23:54:44 -0500

    Update app_v2.js

--- app_v2.js
+++ app_v2.js
@@ -5,7 +5,7 @@
 // ✅ URL del TRIGGER HTTP del flujo "Generar MCI"
 // (copiada directamente desde Power Automate, con & normales)
 const FLOW_GET_ONEDRIVE_FILE =
-  "https://defaulte4e1bc33e2834312bb3789010224b7.fe.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/dc99f30c70a64d57b309dce1c13d1290/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=NMxJMh4pAr98EPpIwDJGzHb5_glsVkAv-A1TVjR9zsA";
+  "https://defaulte4e1bc33e2834312bb3789010224b7.fe.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/bd9e2227be594ecdb47c0da4a898d474/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=j3SlbYcxilxwhnHJfL95lpTA-Y2RzAtiNrmug_D01eQ";
 
 /* ======================================================================
    0) IMPORTS — NECESARIOS
