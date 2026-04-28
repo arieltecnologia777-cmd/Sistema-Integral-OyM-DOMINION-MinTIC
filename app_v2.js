a8ea9e4e6996f65d90143e55aff55df828089aa3
arieltecnologia777-cmd <arieltecnologia777@gmail.com>
2026-04-28 12:39:55 -0500

    Update app_v2.js

--- app_v2.js
+++ app_v2.js
@@ -7,9 +7,6 @@
 const FLOW_FOTOS =
   "https://defaulte4e1bc33e2834312bb3789010224b7.fe.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/e5f65d8cc4aa4001b6966552ed454170/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ybNuejYFtJf4p_P2vNPf_TY_Zzm2uvkSVYkqPu0GyQg";
 
-const FLOW_MOVER =
-  "https://defaulte4e1bc33e2834312bb3789010224b7.fe.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/2f8dc5e01ad44292a53f0fd9b6804f75/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=THpnzSYpkHb6Sbg3uwHumruzolwNcvkN3Rw-YfvOj6A";
-
 async function tienePermisoOneDrive() {
   try {
     const token = await obtenerToken();
@@ -888,16 +885,6 @@ document.getElementById("visorAprobar").addEventListener("click", async () => {
     }
   );
 
-  // ✅ Avisar a Power Automate para mover archivos
-  await fetch(FLOW_MOVER, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      accion: "aprobado",
-      fileIdentifierExcel: item.fileIdentifierExcel,
-      jsonFileId: item.jsonFileId
-    })
-  });
 
   // ✅ Cerrar modal y refrescar tabla (UNA SOLA VEZ)
   await cargarDatosModulo();
@@ -948,17 +935,6 @@ document.getElementById("visorRechazar").addEventListener("click", async () => {
       })
     }
   );
-
-  // ✅ Avisar a Power Automate para mover archivos
-  await fetch(FLOW_MOVER, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      accion: "rechazado",
-      fileIdentifierExcel: item.fileIdentifierExcel,
-      jsonFileId: item.jsonFileId
-    })
-  });
 
   // ✅ Cerrar modal y refrescar tabla (UNA SOLA VEZ)
   await cargarDatosModulo();
