61216384d763eebfeea2385afc6f5b2de26c1172
arieltecnologia777-cmd <arieltecnologia777@gmail.com>
2026-04-28 23:12:46 -0500

    Update tecnicos.js

--- tecnicos.js
+++ tecnicos.js
@@ -1,1 +1,10 @@
+// tecnicos.js
+
+// Contraseña única para todos los técnicos
+export const PASSWORD_GLOBAL = "m1nt1c*#7";
+
+// Mapa: CÉDULA → NOMBRE COMPLETO
+export const TECNICOS = {
+  "1234567": "USUARIO PRUEBA"
+};
 
