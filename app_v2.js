2b358e32b1156f16690194fe6dc5807550eacffe
arieltecnologia777-cmd <arieltecnologia777@gmail.com>
2026-05-01 23:48:18 -0500

    Update app_v2.js

--- app_v2.js
+++ app_v2.js
@@ -259,7 +259,7 @@ async function seleccionarModulo(mod) {
     <img
   src="./auditor-dashboard.png" 
   style="
-    width:520px;
+    width:650px;
     position:relative;
     z-index:10; /* ✅ IMAGEN MÁXIMA PRIORIDAD */
     transform: translateX(20px); /* ✅ la hace sobresalir */
