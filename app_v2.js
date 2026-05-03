5ca621447997207a4c87ea62b67d50f031c28195
arieltecnologia777-cmd <arieltecnologia777@gmail.com>
2026-05-02 23:52:22 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app_v2.js
+++ app_v2.js
@@ -259,7 +259,7 @@ async function seleccionarModulo(mod) {
     <img
   src="./auditor-dashboard.png" 
   style="
-    width:280px;
+    width:250px;
     position:relative;
     z-index:10; /* ✅ IMAGEN MÁXIMA PRIORIDAD */
     transform: translateX(20px); /* ✅ la hace sobresalir */
